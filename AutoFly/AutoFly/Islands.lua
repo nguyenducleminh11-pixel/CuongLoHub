e95f42b4e60081deecc8168fcfe56990c7b3f178
nguyenducleminh11-pixel <nguyenducleminh11@gmail.com>
2026-02-01 10:33:11 +0700

    Create Islands.lua

---
+++ AutoFly/AutoFly/Islands.lua
@@ -0,0 +1,43 @@
+return {
+
+    Sea1 = {
+        StarterIsland   = Vector3.new(64, 10, 144),
+        Jungle          = Vector3.new(-128, 15, 256),
+        PirateVillage   = Vector3.new(480, 15, -320),
+        Desert          = Vector3.new(1120, 15, 4320),
+        FrozenVillage   = Vector3.new(-800, 15, 2300),
+        Marineford      = Vector3.new(1550, 55, -1150),
+        SkyIsland       = Vector3.new(-4500, 1800, -1800),
+        Prison          = Vector3.new(-3500, 15, 2800),
+        Colosseum       = Vector3.new(-1400, 15, -600),
+        MagmaVillage    = Vector3.new(3000, 15, 3200),
+        UnderwaterCity  = Vector3.new(3900, -90, -1900),
+        FountainCity    = Vector3.new(2200, 20, -1900),
+    },
+
+    Sea2 = {
+        KingdomOfRose   = Vector3.new(1700, 30, -200),
+        GreenZone       = Vector3.new(-2200, 30, -3100),
+        Graveyard       = Vector3.new(2300, 30, 1200),
+        SnowMountain    = Vector3.new(3500, 70, -5500),
+        HotAndCold      = Vector3.new(-5600, 90, -5077),
+        CursedShip      = Vector3.new(-1800, 15, -1600),
+        IceCastle       = Vector3.new(5400, 80, -6200),
+        ForgottenIsland = Vector3.new(4600, 30, -3100),
+        DarkArena       = Vector3.new(1200, 30, -1100),
+        UsoppIsland     = Vector3.new(-1900, 30, 600),
+    },
+
+    Sea3 = {
+        PortTown        = Vector3.new(-2900, 20, 5300),
+        HydraIsland     = Vector3.new(5200, 600, -100),
+        GreatTree       = Vector3.new(2300, 25, -650),
+        FloatingTurtle  = Vector3.new(100, 50, -7800),
+        CastleOnTheSea  = Vector3.new(-5500, 310, -800),
+        HauntedCastle  = Vector3.new(3800, 40, -4500),
+        SeaOfTreats    = Vector3.new(2200, 15, -3900),
+        TikiOutpost    = Vector3.new(-4200, 25, -4200),
+        Mansion        = Vector3.new(-1250, 30, -1450),
+    }
+
+}
